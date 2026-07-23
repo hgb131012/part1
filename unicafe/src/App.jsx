5802407c5d5acaf24d61adb8110b107fe5258d80
Gia Bao <hgb131012@gmail.com>
2026-07-23 17:19:10 +0700

    Completed 1.6 and 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,122 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = ({ title, style }) => {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <header>
+      <h1 style={style}>{title}</h1>
+    </header>
   )
 }
 
-export default App
+const Button = ({ onClick, content, style }) => {
+  return (
+    <button style={style} onClick={onClick}>{content}</button>
+  )
+}
+
+const StatisticsHeader = ({ title, style }) => {
+  return (
+    <h2 style={style}>{title}</h2>
+  )
+}
+
+const StatisticsDisplay = ({ goodNum, neutralNum, badNum, totalNum, average, positive, style }) => {
+  return (
+    <section>
+      <p style={style}>good {goodNum}</p>
+      <p style={style}>neutral {neutralNum}</p>
+      <p style={style}>bad {badNum}</p>
+      <p style={style}>all {totalNum}</p>
+      <p style={style}>average {average}</p>
+      <p style={style}>postive {positive} %</p>
+    </section>
+    
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [score, setScore] = useState(0);
+
+  return (
+    <main>
+      <Header title="give feedback" style={{fontFamily: 'Arial, Helvetica, sans-serif'}} />
+      <Button onClick={() => {setGood(g => g + 1); setScore(s => s + 1)}} content="good" style={{fontSize: '16px', margin: '5px'}} />
+      <Button onClick={() => setNeutral(n => n + 1)} content="neutral" style={{fontSize: '16px', margin: '5px'}} />
+      <Button onClick={() => {setBad(b => b + 1); setScore(s => s - 1)}} content="bad" style={{fontSize: '16px', margin: '5px'}} />
+      <StatisticsHeader title="statistics" style={{fontFamily: 'Arial, Helvetica, sans-serif'}} />
+      <StatisticsDisplay goodNum={good} neutralNum={neutral} badNum={bad} totalNum={bad + good + neutral} positive={(good / (bad + good + neutral)) * 100} average={score / (bad + good + neutral)} style={{fontFamily: 'Arial, Helvetica, sans-serif'}}/>
+    </main>
+  )
+}
+
+export default App;
