284d7acf9778c24a0be478c25019c0b4367fd55a
Gia Bao <hgb131012@gmail.com>
2026-07-26 17:58:50 +0700

    Completed 2.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,47 +1,4 @@
-const CourseHeader = ({ title }) => {
-  return (
-    <header>
-      <h1>{title}</h1>
-    </header>
-  )
-}
-
-const Total = (props) => {
-  const total = props.part.reduce((sum, current) => sum + current.exercises, 0)
-  return (
-    <h3>total of {total} exercises</h3>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      {props.courses.map((course) => {
-        return (
-          <section key={course.id}>
-            <h2>{course.name}</h2>
-            {course.parts.map((part) => {
-              return (
-                <p key={part.id}>{part.name} {part.exercises}</p>
-              )
-            })}
-            <Total part={course.parts} />
-          </section>
-        )
-      })}
-    </div>
-  )
-  
-}
-
-const Course = (props) => {
-  return (
-    <main>
-      <CourseHeader title="Web development curriculum" />
-      <Part courses={props.courses} />
-    </main>
-  )
-}
+import Course from './Course.jsx'
 
 const App = () => {  
   const courses = [
